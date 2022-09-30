ce7fb4d71eb15b3fd18e2bf57fdb8366d168ad39
hemanthk-amagi <hemanth.krishna@amagi.com>
2022-09-30 22:38:13 +0530

    Finding Unique User Ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+
+  userIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
